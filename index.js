17d0e6caa2b5b853b6471750b0f028c4e3d0c24a
Jarod Wilson <jaroddwilson@gmail.com>
2022-01-14 13:31:10 -0500

    commit tree d247e2b2659ed23ac72f3d28764f961162e999f6

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.replace(string.charAt(0), string.charAt(0).toUpperCase());
+  }
+
+  static sanitize(string) {
+    const regex = /[^A-Za-z0-9\'\-\s]/g
+    return string.replaceAll(regex, '')
+  }
+
+  static titleize(string) {
+    const titleString = Formatter.capitalize(string);
+    const words = titleString.split(' ');
+    let outputString = ''
+    for (let word of words) {
+      switch (word) {
+        case 'the':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'a':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'an':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'but':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'of':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'and':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'for':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'at':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'by':
+          outputString = outputString.concat(' ', word);
+          break;
+        case 'from':
+          outputString = outputString.concat(' ', word);
+          break;
+        default:
+          outputString = outputString.concat(' ', Formatter.capitalize(word));
+      }
+    }
+    return outputString.trimStart();
+  }
 }
